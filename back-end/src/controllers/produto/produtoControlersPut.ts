fecfa34b89bd8fd340552adbcc1d3728479e66e3
Wesllen Vinicius <wesllenvinicius09@gmail.com>
2022-04-30 20:29:43 -0400

    implementando-metodo-put-apis

---
+++ back-end/src/controllers/produto/produtoControlersPut.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express"
+import { prismaClient } from "../../data/prismaClient"
+
+class ProdutoControlersPut {
+  static async updateProdutos(_req: Request, res: Response) {
+    try {
+      const { id, sal_fino, sal_grosso, metabissulfito, peroxido, bombonas } =
+        _req.params
+      const postData = await prismaClient.produto.findUnique({
+        where: { id: Number(id) },
+        select: {
+          sal_fino,
+          sal_grosso,
+          metabissulfito,
+          peroxido,
+          bombonas,
+        },
+      })
+      const updatedProduto = await prismaClient.produto.update({
+        where: { id: Number(id) || undefined },
+      })
+      res.json(updatedProduto)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json({
+        error: "O Produto de Id informado, não existe na tabela Produtos!",
+      })
+    }
+  }
+}
+export default ProdutoControlersPut
