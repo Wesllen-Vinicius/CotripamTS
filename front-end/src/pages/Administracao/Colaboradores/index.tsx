996d6f4bd3f7630229d334b53ec7445f893c60de
Wesllen <wesllen@tjro.jus.br>
2022-05-11 09:51:01 -0400

    tela-administração

--- front-end/src/pages/Administracao/Colaboradores/index.tsx
+++ front-end/src/pages/Administracao/Colaboradores/index.tsx
@@ -1,12 +1,60 @@
+import axios from "axios"
+import { useForm } from "react-hook-form"
+import { SubmitHandler, UseFormRegister } from "react-hook-form/dist/types/form"
+import { Path } from "react-hook-form/dist/types/path/eager"
 import CardGlobalComponent from "../../../components/GlobalComponents/cardGlobalComponent/CardGlobal"
+import FormGlobalComponent from "../../../components/GlobalComponents/formGlobalComponent/formGlobalComponent"
 import AdmLayout from "../../../components/layouts/AdmLayout"
 
+
+
+interface IFormValues {
+  Nome: String
+  Email: String
+  PassWord: String
+  NivelAcesso: Boolean
+}
+
+type InputProps = {
+  label: Path<IFormValues>
+  register: UseFormRegister<IFormValues>
+  required: boolean
+}
+
+const Input = ({ label, register, required }: InputProps) => (
+  <>
+    <label>{label}</label>
+    <input {...register(label, { required })} />
+  </>
+)
+
+
 export default function Colaboradores() {
+  const { register, handleSubmit } = useForm<IFormValues>()
+
+  localStorage.getItem("@App:user")
+  const userJson = JSON.parse(window.localStorage.getItem("@App:user") || "{}")
+  window.localStorage.getItem("@App:user")
+  const userId = userJson.id
+
+  const onSubmit: SubmitHandler<IFormValues> = (data) =>
+    axios.post("", data, userId)
+
+
   return (
     <AdmLayout>
-      <CardGlobalComponent>
-        <h1>Colaboradores</h1>
-      </CardGlobalComponent>
+    <CardGlobalComponent>
+      <FormGlobalComponent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1>Cadastro de Colaboradores</h1>
+          <Input label="Nome" register={register} required />
+          <Input label="Email" register={register} required />
+          <Input label="PassWord" register={register} required />
+          <Input label="NivelAcesso" register={register} required />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </FormGlobalComponent>
+    </CardGlobalComponent>
     </AdmLayout>
   )
 }
