2342fdff02aaaf64e4106a44781d7c71bd595eb3
Wesllen <wesllen@tjro.jus.br>
2022-05-11 09:57:27 -0400

    tela-administração

--- front-end/src/pages/Administracao/Relatorios/index.tsx
+++ front-end/src/pages/Administracao/Relatorios/index.tsx
@@ -1,11 +1,32 @@
 import CardGlobalComponent from "../../../components/GlobalComponents/cardGlobalComponent/CardGlobal"
+import TableGlobalComponent from "../../../components/GlobalComponents/TableGlobalComponent/TableGlobal"
 import AdmLayout from "../../../components/layouts/AdmLayout"
 
 export default function Relatorios() {
   return (
     <AdmLayout>
       <CardGlobalComponent>
-        <h1>Relatorios</h1>
+       <TableGlobalComponent>
+       <div>
+          <h1>Historico</h1>
+          <thead>
+            <tr>
+              <th>Total Abate</th>
+              <th>Data</th>
+              <th>Meta Dia</th>
+              <th>Meta Esperada</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>00,00</td>
+              <td>00/00/00</td>
+              <td>0,00</td>
+              <td>0,00</td>
+            </tr>
+          </tbody>
+        </div>
+       </TableGlobalComponent>
       </CardGlobalComponent>
     </AdmLayout>
   )
